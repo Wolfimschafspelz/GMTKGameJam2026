ab38cf33a209ff2618b49cd47ca29b4a27b75623
AlbrErik <erik.albrecht@htp-tel.de>
2026-07-24 22:06:49 +0200

    Optimize selection

--- ui_layer.gd
+++ ui_layer.gd
@@ -1,13 +1,15 @@
 extends TileMapLayer
 
-const test_scene = preload("res://test.tscn")
+@onready var path_layer: TileMapLayer = $"../PathLayer"
 
 var is_active = false # control selection ui
 var last_hover = null # the last mouse hover position
+var path_neighbors: Array[Vector2i] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	is_active = true
+	path_neighbors = get_path_neighbors()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,17 +20,29 @@ func _input(event):
 	if is_active:
 		if event is InputEventMouseButton:
 			if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-				pass # todo: load selected tower and place it at map_to_local(get_hovered_tile)
+				if get_hovered_tile() in path_neighbors:
+					print("Valid!")
+					pass # todo: load selected tower and place it at map_to_local(get_hovered_tile)
 
 func on_hover_tile():
 	var cur_hover = get_hovered_tile()
-	if last_hover != null and (last_hover != cur_hover):
-		# set_cell(coords, source_id, atlas_coords, alternative tile)
-		set_cell(cur_hover, 0, Vector2i(0,0), 0)
-		erase_cell(last_hover)		
-	elif last_hover == null:
-		set_cell(cur_hover, 0, Vector2i(0,0), 0)		
-	last_hover = cur_hover
+	if last_hover != cur_hover:	
+		if last_hover != null:
+			erase_cell(last_hover)		
+		if cur_hover in get_path_neighbors():
+			# set_cell(coords, source_id, atlas_coords, alternative tile)
+			set_cell(cur_hover, 0, Vector2i(0,0), 0)
+		last_hover = cur_hover
 
 func get_hovered_tile():
 	return local_to_map(get_local_mouse_position())
+
+func get_path_neighbors():
+	var NEIGHBOR_TYPES = [TileSet.CELL_NEIGHBOR_RIGHT_SIDE, TileSet.CELL_NEIGHBOR_LEFT_SIDE, TileSet.CELL_NEIGHBOR_TOP_SIDE, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE]
+	var neighbors: Array[Vector2i] = []
+	for tile in path_layer.get_used_cells():
+		for type in NEIGHBOR_TYPES:
+			var cur_neighbor = path_layer.get_neighbor_cell(tile, type)
+			if cur_neighbor not in neighbors and cur_neighbor not in path_layer.get_used_cells():
+				neighbors.append(cur_neighbor)
+	return neighbors
