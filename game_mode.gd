713b2c1f2ffd0e86349ff8ee97d74c1e80055e0d
Lukas Steinmann <lukas.steinmann@gmx.de>
2026-07-24 23:56:42 +0200

    Implemented health and point interfaces

---
+++ game_mode.gd
@@ -0,0 +1,36 @@
+extends Node
+
+const max_health = 100
+var health: int
+var points: int
+
+# TODO: Wave management
+
+func withdraw_points(amount: int):
+	points += amount
+
+func can_buy(price: int) -> bool:
+	return points >= price
+
+func deposit_points(amount: int):
+	if points - amount <= 0:
+		pass
+	points -= amount
+
+func die():
+	# TODO: implement me
+	pass
+
+func take_damage(amount: int):
+	health -= amount
+	if health <= 0:
+		die()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	points = 0
+	health = max_health
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
